211a8493bbc36b4dbd49e3bba72219409f6c4c2f
Pedro Erick Lopez Camacho <ericklopezc101@hotmail.com>
2023-09-18 11:04:13 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoHotel/Logica/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=DESKTOP-SKD156L\\SQLEXPRESS;Initial Catalog=DB_HOTEL;Integrated Security=True";
+    }
+  //  Initial Catalog = DB_HOTEL; Integrated Security = True
+}
